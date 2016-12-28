2d34279ce230854b9cedcf32f854a870b701541d
smutnyjoe <marbit12@gmail.com>
2016-12-28 10:46:04 +0100

    added Captain to ASD

--- Alghoritms and data structures/Captain/captain.cpp
+++ Alghoritms and data structures/Captain/captain.cpp
@@ -5,7 +5,6 @@
 #include <forward_list>
 #include <queue>
 
-using namespace std;
 
 typedef std::tuple<int, int, int, std::forward_list<int>, int> island; // <x, y, no, list_of_neighbours, distance>
 
@@ -32,46 +31,46 @@ int main()
     std::sort(islands.begin(), islands.end(),
               [] (const island& lhs, const island & rhs)
               {
-                  if (get<0>(lhs) != get<0>(rhs)) // sort by x...
-                      return get<0>(lhs) < get<0>(rhs);
+                  if (std::get<0>(lhs) != std::get<0>(rhs)) // sort by x...
+                      return std::get<0>(lhs) < std::get<0>(rhs);
                   else
-                      return get<1>(lhs) < get<1>(rhs); // then by y
+                      return std::get<1>(lhs) < std::get<1>(rhs); // then by y
               });
 
     // connect closest neighbours by x: (add numbers of x-neighbours to neighbour list
-    get<3>(islands[0]).push_front((int &&) get<2>(islands[1]));
+    std::get<3>(islands[0]).push_front((int &&) std::get<2>(islands[1]));
     for(int i = 1; i < n - 1; i++) {
-        get<3>(islands[i]).push_front((int &&) get<2>(islands[i - 1]));
-        get<3>(islands[i]).push_front((int &&) get<2>(islands[i + 1]));
+        std::get<3>(islands[i]).push_front((int &&) std::get<2>(islands[i - 1]));
+        std::get<3>(islands[i]).push_front((int &&) std::get<2>(islands[i + 1]));
     }
-    get<3>(islands[n - 1]).push_front((int &&) get<2>(islands[n - 2]));
+    std::get<3>(islands[n - 1]).push_front((int &&) std::get<2>(islands[n - 2]));
 
     // sort by y:
     std::sort(islands.begin(), islands.end(),
               [] (const island& lhs, const island & rhs)
               {
-                  if (get<1>(lhs) != get<1>(rhs)) // sort by y...
-                      return get<1>(lhs) < get<1>(rhs);
+                  if (std::get<1>(lhs) != std::get<1>(rhs)) // sort by y...
+                      return std::get<1>(lhs) < std::get<1>(rhs);
                   else
-                      return get<0>(lhs) < get<0>(rhs); // then by x
+                      return std::get<0>(lhs) < std::get<0>(rhs); // then by x
               });
 
 
 
     // connect closest neighbours by y: (add numbers of y-neighbours to neighbour list
-    get<3>(islands[0]).push_front((int &&) get<2>(islands[1]));
+    std::get<3>(islands[0]).push_front((int &&) std::get<2>(islands[1]));
     for(int i = 1; i < n - 1; i++) {
-        get<3>(islands[i]).push_front((int &&) get<2>(islands[i - 1]));
-        get<3>(islands[i]).push_front((int &&) get<2>(islands[i + 1]));
+        std::get<3>(islands[i]).push_front((int &&) std::get<2>(islands[i - 1]));
+        std::get<3>(islands[i]).push_front((int &&) std::get<2>(islands[i + 1]));
     }
-    get<3>(islands[n - 1]).push_front((int &&) get<2>(islands[n - 2]));
+    std::get<3>(islands[n - 1]).push_front((int &&) std::get<2>(islands[n - 2]));
 
 
     // sort islands by their original order (by number)
     std::sort(islands.begin(), islands.end(),
               [] (const island& lhs, const island& rhs)
               {
-                  return get<2>(lhs) < get<2>(rhs);
+                  return std::get<2>(lhs) < std::get<2>(rhs);
               });
 
 
@@ -102,4 +101,5 @@ int main()
     std::cout << std::get<4>(islands[n - 1]) << std::endl;
 
 }
+
 
